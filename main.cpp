7fad6fd427923e9f1bcb54f0e29794cae9050816
Jo <josve@stid.ntnu.no>
2023-03-27 09:55:31 +0200

    Grid and Box

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 
 #include "threepp/threepp.hpp"
-#include "threepp/extras/imgui/imgui_context.hpp"
+
 
 using namespace threepp;
 
@@ -8,7 +8,7 @@ int main() {
 
     Canvas canvas;
     GLRenderer renderer(canvas);
-    renderer.setClearColor(Color::aliceblue);
+    renderer.setClearColor(Color::papayawhip);
 
     auto camera = PerspectiveCamera::create();
     camera->position.z = 5;
@@ -17,56 +17,33 @@ int main() {
 
     auto scene = Scene::create();
 
+    auto grid = GridHelper::create(20, 10, Color::red);
+    scene->add(grid);
+
     auto group = Group::create();
     scene->add(group);
-
-    {
-        auto geometry = ConeGeometry::create();
-        auto material = MeshBasicMaterial::create();
-        material->color = Color::purple;
-        auto mesh = Mesh::create(geometry, material);
-        mesh->position.x = -1;
-        group->add(mesh);
-    }
-
-    {
-        auto geometry = PlaneGeometry::create();
-        auto material = MeshBasicMaterial::create();
-        material->color = Color::blue;
-        auto mesh = Mesh::create(geometry, material);
-        mesh->position.x = 1;
-        group->add(mesh);
-    }
-
-    renderer.enableTextRendering();
-    auto& textHandle = renderer.textHandle("Hello World");
-    textHandle.setPosition(0, canvas.getSize().height-30);
-    textHandle.scale = 2;
-
-
-    std::array<float, 3> posBuf{};
-    imgui_functional_context ui(canvas.window_ptr(), [&] {
-        ImGui::SetNextWindowPos({0, 0}, 0, {0, 0});
-        ImGui::SetNextWindowSize({230, 0}, 0);
-        ImGui::Begin("Demo");
-        ImGui::SliderFloat3("position", posBuf.data(), -1.f, 1.f);
-        controls.enabled = !ImGui::IsWindowHovered();
-        ImGui::End();
-    });
 
     canvas.onWindowResize([&](WindowSize size){
         camera->aspect = size.getAspect();
         camera->updateProjectionMatrix();
         renderer.setSize(size);
-        textHandle.setPosition(0, size.height-30);
+//        textHandle.setPosition(0, size.height-30);
     });
 
+
     canvas.animate([&] {
-
         renderer.render(scene, camera);
+    {
+        auto geometry = BoxGeometry::create();
+        auto material = MeshBasicMaterial::create();
+        material->color = Color::purple;
+        auto mesh = Mesh::create(geometry, material);
+        mesh->position.x = 1;
+        mesh->position.y = 0.5;
+        mesh->position.z = 1;
+        group->add(mesh);
+    }
 
-        ui.render();
-        group->position.fromArray(posBuf);
 
     });
 
