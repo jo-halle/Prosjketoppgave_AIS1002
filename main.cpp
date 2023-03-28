ed7490521632c8d760c2825b92f3d570c3729162
Jo <josve@stid.ntnu.no>
2023-03-28 12:04:10 +0200

    Grid and Box without anything else

--- main.cpp
+++ main.cpp
@@ -1,6 +1,4 @@
-
 #include "threepp/threepp.hpp"
-
 
 using namespace threepp;
 
@@ -11,7 +9,8 @@ int main() {
     renderer.setClearColor(Color::papayawhip);
 
     auto camera = PerspectiveCamera::create();
-    camera->position.z = 5;
+    camera->position.z = 10;
+    camera->position.y = 10;
 
     OrbitControls controls{camera, canvas};
 
@@ -23,28 +22,23 @@ int main() {
     auto group = Group::create();
     scene->add(group);
 
+    auto geometry = BoxGeometry::create();
+    auto material = MeshBasicMaterial::create();
+    material->color = Color::purple;
+    auto mesh = Mesh::create(geometry, material);
+    mesh->position.x = 1;
+    mesh->position.y = 0.5;
+    mesh->position.z = 1;
+    group->add(mesh);
+
     canvas.onWindowResize([&](WindowSize size){
         camera->aspect = size.getAspect();
         camera->updateProjectionMatrix();
         renderer.setSize(size);
-//        textHandle.setPosition(0, size.height-30);
     });
-
 
     canvas.animate([&] {
         renderer.render(scene, camera);
-    {
-        auto geometry = BoxGeometry::create();
-        auto material = MeshBasicMaterial::create();
-        material->color = Color::purple;
-        auto mesh = Mesh::create(geometry, material);
-        mesh->position.x = 1;
-        mesh->position.y = 0.5;
-        mesh->position.z = 1;
-        group->add(mesh);
-    }
-
-
     });
 
 }
