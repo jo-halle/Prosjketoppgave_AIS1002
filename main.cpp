d26dccdabe86a33598105c5c1a8e097453e20e80
Jo <josve@stid.ntnu.no>
2023-04-19 13:27:46 +0200

    Movement done! With classes(og nå riktig main.cpp..)!

--- main.cpp
+++ main.cpp
@@ -1,70 +1,11 @@
 #include "threepp/threepp.hpp"
+#include "game.hpp"
+#include "box.hpp"
+
 
 using namespace threepp;
 
-enum class Direction {
-    LEFT,
-    RIGHT,
-    UP,
-    DOWN,
-    REST
-};
-
-class Game : public KeyListener {
-public:
-    Direction nextDirection;
-    bool running;
-    bool shouldMove;
-
-    Game() : nextDirection(Direction::LEFT), running(false), shouldMove(false) {}
-
-    void start() {
-
-        nextDirection = Direction::LEFT;
-        running = true;
-        shouldMove = false;
-    }
-
-
-    bool isRunning() const {
-        return running;
-    }
-
-    void reset() {
-        nextDirection = Direction::LEFT;
-        running = false;
-        shouldMove = false;
-    }
-
-    void onKeyPressed(KeyEvent evt) override {
-        if (evt.key == 87 /*W*/) {
-            nextDirection = Direction::UP;
-            shouldMove = true;
-        }
-        if (evt.key == 83 /*S*/) {
-            nextDirection = Direction::DOWN;
-            shouldMove = true;
-        }
-        if (evt.key == 65 /*A*/) {
-            nextDirection = Direction::LEFT;
-            shouldMove = true;
-        }
-        if (evt.key == 68 /*D*/) {
-            nextDirection = Direction::RIGHT;
-            shouldMove = true;
-        }
-        if (evt.key == 82 /*R*/) {
-            reset();
-            start();
-        }
-//        if (evt.key == 32 /*SPACE*/) {
-//            nextDirection = Direction::REST;
-//        }
-    }
-};
-
 int main() {
-
     Canvas canvas;
     GLRenderer renderer(canvas);
     renderer.setClearColor(Color::papayawhip);
@@ -82,18 +23,10 @@ int main() {
     auto grid = GridHelper::create(20, 10, Color::red);
     scene->add(grid);
 
-    auto group = Group::create();
-    scene->add(group);
+    auto box = Box(*scene);
 
-    auto geometry = BoxGeometry::create();
-    auto material = MeshBasicMaterial::create();
-    material->color = Color::purple;
-    auto mesh = Mesh::create(geometry, material);
-    mesh->position.x = 0;
-    mesh->position.y = 0.5;
-    mesh->position.z = 0;
-    auto startingPosition = Vector3(mesh->position.x, mesh->position.y, mesh->position.z);
-    group->add(mesh);
+    int rows = 10;
+    int cols = 10;
 
     canvas.onWindowResize([&](WindowSize size) {
         camera->aspect = size.getAspect();
@@ -106,16 +39,16 @@ int main() {
             if (game.shouldMove) {
                 switch (game.nextDirection) {
                     case Direction::LEFT:
-                        mesh->position.x -= 2.0;
+                        box.getMesh()->position.x -= 2.0;
                         break;
                     case Direction::RIGHT:
-                        mesh->position.x += 2.0;
+                        box.getMesh()->position.x += 2.0;
                         break;
                     case Direction::UP:
-                        mesh->position.z -= 2.0;
+                        box.getMesh()->position.z -= 2.0;
                         break;
                     case Direction::DOWN:
-                        mesh->position.z += 2.0;
+                        box.getMesh()->position.z += 2.0;
                         break;
                     case Direction::REST:
                         break;
@@ -123,9 +56,8 @@ int main() {
                         break;
                 }
 
-                // Check if the box has moved one unit
-                if (abs(mesh->position.x - startingPosition.x) >= 2.0 ||
-                    abs(mesh->position.z - startingPosition.z) >= 2.0) {
+                if (abs(box.getMesh()->position.x - box.getStartingPosition().x) >= 2.0 ||
+                    abs(box.getMesh()->position.z - box.getStartingPosition().z) >= 2.0) {
                     game.shouldMove = false;
                 }
             }
